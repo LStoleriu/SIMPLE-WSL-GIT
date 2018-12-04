e832586fd2d5403e784513af5cfd448700d43832
LStoleriu <smartassacus@gmail.com>
2018-12-04 18:59:48 +0200

    new commint - minor changes

--- simplecpp.cpp
+++ simplecpp.cpp
@@ -6,7 +6,7 @@ int main()
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     std::normal_distribution<> norm_dis(0.0, 0.3);
-    std::uniform_int_distribution<> dis(1, 6);
+    std::uniform_real_distribution<double> dis(0, 1);
  
     for (int n=0; n<20; ++n)
         std::cout << dis(gen) << ' ';
