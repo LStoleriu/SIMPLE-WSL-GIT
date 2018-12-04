8ad3ebadaf39b8ee0a143996105d802ea9cea161
LStoleriu <smartassacus@gmail.com>
2018-12-04 19:05:47 +0200

    test normal distrib

--- simplecpp.cpp
+++ simplecpp.cpp
@@ -3,9 +3,9 @@
  
 int main()
 {
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::normal_distribution<> norm_dis(0.0, 0.3);
+    std::random_device rd{};  //Will be used to obtain a seed for the random number engine
+    std::mt19937_64 gen{rd()}; //Standard mersenne_twister_engine seeded with rd()
+    std::normal_distribution<double> norm_dis(0, 0.1);
     std::uniform_real_distribution<double> dis(0, 1);
  
     for (int n=0; n<20; ++n)
@@ -13,7 +13,7 @@ int main()
 
     std::cout << '\n';
 
-    for (int n=0; n<5; ++n)
+    for (int n=0; n<12; ++n)
         std::cout << norm_dis(gen) << ' ';
         
     std::cout << '\n';
